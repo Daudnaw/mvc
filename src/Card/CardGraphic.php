adc248926c5fd48b235bb479458b4ca1664e4180
Daud Nawaz <dana23@student.bth.se>
2025-04-25 22:56:11 +0200

    Add docblock

--- src/Card/CardGraphic.php
+++ src/Card/CardGraphic.php
@@ -2,6 +2,12 @@
 
 namespace App\Card;
 
+/**
+ * Class CardGraphic
+ * 
+ * This class inherits from the Card class.
+ * This class gives a card symbolic representation.
+ */
 class CardGraphic extends Card
 {
     /**
@@ -14,16 +20,32 @@ class CardGraphic extends Card
         'clubs' => '♣',
     ];
 
+    /**
+     * Create a card with symbolic suit '♥' instead of string 'hearts'.
+     * 
+     * @param string $suit cards suit ex. 'clubs'.
+     * @param string $rank cards rank ex. 'A'.
+     */
     public function __construct(string $suit, string $rank)
     {
         parent::__construct($suit, $rank);
     }
 
+    /**
+     * Gets the symbol for a card suit.
+     * 
+     * @return string Graphic symbol for a suit.
+     */
     public function getSuitSymbol(): string
     {
         return $this->suits[$this->suit] ?? '';
     }
 
+    /**
+     * Returns a card with graphicsymbol ex. '[A♥]'.
+     * 
+     * @return string Card as string in graphic form
+     */
     public function getAsString(): string
     {
         return '[' . $this->rank . $this->getSuitSymbol() . ']';
