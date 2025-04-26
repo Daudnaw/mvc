68c6d22643212ec25820e17fba181b5e613a3b9d
Daud Nawaz <dana23@student.bth.se>
2025-04-26 23:52:31 +0200

    Add blockcomments

--- src/Card/Game.php
+++ src/Card/Game.php
@@ -4,13 +4,26 @@ namespace App\Card;
 
 use App\Card\Deck;
 
+/**
+ * Class Game
+ *
+ * This class inherits from Deck and some function to play twentyOne
+ */
 class Game extends Deck
 {
+    /**
+     * Creates new Deck of 52 cards without jokers
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * This function gives rank as integer and also a number for A, K, Q and J
+     *
+     * @return int $rank as an integer
+     */
     public function getRank(Card $card): int
     {
         $rank = $card->getRank();
@@ -25,6 +38,13 @@ class Game extends Deck
         return $cardValues[$rank] ?? (int) $rank;
     }
 
+    /**
+     * Decider of the winer of the game 21
+     *
+     * @param int $spelare points spelare has collected.
+     * @param int $bank points bank has collected.
+     * @return string returns a string bank or spelare
+     */
     public function winLose(int $spelare, int $bank): string
     {
         if ($bank > 21) {
