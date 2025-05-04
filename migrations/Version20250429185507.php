211c986b47099d9b8c8f1c9c9a0f2828232a65d5
Daud Nawaz <dana23@student.bth.se>
2025-05-04 12:43:53 +0200

    Escape phpmd

--- migrations/Version20250429185507.php
+++ migrations/Version20250429185507.php
@@ -9,7 +9,10 @@ use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
+ * 
+ * @SuppressWarnings(PHPMD)
  */
+
 final class Version20250429185507 extends AbstractMigration
 {
     public function getDescription(): string
