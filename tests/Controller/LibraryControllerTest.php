d9f33f257e20e0802e5bf0ad869beab5cd516ce3
Daud Nawaz <dana23@student.bth.se>
2025-05-10 00:06:53 +0200

    Add dummytest

---
+++ tests/Controller/LibraryControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class LibraryControllerTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
